150a8367973e5948e963b3d1d30a615e2d513cd3
HiagoPeroni <hiago.peroni@moovus.com.br>
2025-04-21 11:14:25 -0300

    feature: 3.1 test done

---
+++ testes-clean-code/teste-3.1.ts
@@ -0,0 +1,30 @@
+import { IItem } from "../interfaces/testes-clean-code/teste-3.1.interface";
+
+function isExpensive(item: IItem): boolean {
+    return item.price > 100;
+};
+
+// Printing to console
+function printIsExpensive(items: IItem[]): void {
+    items.forEach((item) => {
+        if (isExpensive(item)) {
+            console.log(`The item ${item.name} is expensive.`);
+        } else {
+            console.log(`The item ${item.name} is not expensive.`);
+        }
+    });
+}
+
+// Returning a new string
+function stringIsExpensive(items: IItem[]): string {
+    const expensivesList = [] as string[];
+    const notExpensivesList = [] as string[];
+    items.forEach((item) => {
+        if (isExpensive(item)) {
+            expensivesList.push(item.name);
+        } else {
+            notExpensivesList.push(item.name);
+        }
+    });
+    return `The expensive items are: ${expensivesList.join(', ')}. The not expensive items are: ${notExpensivesList.join(', ')}`;
+}
